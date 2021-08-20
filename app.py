d42a1c0f3420c6ce810722a1474a3f63929fbc77
Apisit3 <89236563+Apisit3@users.noreply.github.com>
2021-08-20 12:51:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,13 @@ def hello():
 def get_api():
     return jsonify(data)
 
+@app.route('/hello/<string:name>')
+def Home(name):
+	return render_template('home.html', name_html=name)
+
+@app.route('/name')
+def name():
+    return "<font color=Green>อภิสิทธิ์</font> <font color=pink>ขุนสนธิ</font> <br> <font color=blue>เลขที่3 ม.4/10</font> "
+
 if __name__ == "__main__":
     app.run(debug=False)
